c236f5ef50dfd74cf773eaae91b5f94f4bdbda0e
Raymond Wang <rw5614@gmail.com>
2019-10-20 14:37:25 -0400

    404 and contact mod

--- assets/mail/contact.php
+++ assets/mail/contact.php
@@ -42,7 +42,7 @@ if(get_magic_quotes_gpc()) {
 // Example $address = "joe.doe@yourdomain.com";
 
 //$address = "example@themeforest.net";
-$address = "info@yourdomain.com";
+$address = "hello@civology.com";
 
 
 // Configuration option.
